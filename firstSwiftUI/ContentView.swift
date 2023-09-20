689c76ac493455e38ae12a51e716b983117e1cd3
hank <taiwanhank0908@gmail.com>
2023-09-20 11:03:46 +0800

    Initial Commit

---
+++ firstSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  firstSwiftUI
+//
+//  Created by User19 on 2023/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ firstSwiftUI/firstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstSwiftUIApp.swift
+//  firstSwiftUI
+//
+//  Created by User19 on 2023/9/20.
+//
+
+import SwiftUI
+
+@main
+struct firstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
